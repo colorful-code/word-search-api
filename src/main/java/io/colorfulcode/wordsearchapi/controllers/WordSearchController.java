9311b72dcf6df0c19624eade6de1bbfc9c2c30a1
Amir H <amir@colorfulcode.se>
2023-01-31 21:22:54 +0100

    API for word search game.

---
+++ src/main/java/io/colorfulcode/wordsearchapi/controllers/WordSearchController.java
@@ -0,0 +1,29 @@
+package io.colorfulcode.wordsearchapi.controllers;
+
+import io.colorfulcode.wordsearchapi.services.WordGridService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequestMapping("/wordgrid")
+@RestController()
+public class WordSearchController {
+
+    @Autowired
+    WordGridService wordGridService;
+
+    @GetMapping
+    public String createWordGrid(@RequestParam int gridSize, @RequestParam List<String> words) {
+        char[][] grid = wordGridService.generateGrid(gridSize, words);
+        String gridAsString = "";
+        for(char[] row : grid) {
+            for(char c : row) {
+                gridAsString += c + " ";
+            }
+            gridAsString += "\r\n";
+        }
+        return gridAsString;
+    }
+}
---
+++ src/main/java/io/colorfulcode/wordsearchapi/services/WordGridService.java
@@ -0,0 +1,172 @@
+package io.colorfulcode.wordsearchapi.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+public class WordGridService {
+
+    private class Coordinate {
+        int x;
+        int y;
+
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private enum Direction {
+        HORIZONTAL,
+        VERTICAL,
+        DIAGONAL,
+        HORIZONTAL_INVERSE,
+        VERTICAL_INVERSE,
+        DIAGONAL_INVERSE
+
+    }
+
+    public char[][] generateGrid(int gridSize, List<String> words) {
+        List<Coordinate> coordinates = new ArrayList<>();
+        char[][] contents = new char[gridSize][gridSize];
+        for(int i = 0; i < gridSize; i++) {
+            for(int j = 0; j < gridSize; j++) {
+                coordinates.add(new Coordinate(i, j));
+                contents[i][j] = '_';
+            }
+        }
+
+        Collections.shuffle(coordinates);
+        for(String word: words) {
+            for(Coordinate coordinate : coordinates) {
+                int x = coordinate.x;
+                int y = coordinate.y;
+                Direction selectedDirection = getArbitraryValidDirection(contents, word, coordinate);
+                if(selectedDirection != null) {
+                    switch (selectedDirection) {
+                        case HORIZONTAL -> {
+                            for(char c : word.toCharArray()) {
+                                contents[y][x++] = c;
+                            }
+                        }
+                        case VERTICAL -> {
+                            for(char c : word.toCharArray()) {
+                                contents[y++][x] = c;
+                            }
+                        }
+                        case DIAGONAL -> {
+                            for(char c : word.toCharArray()) {
+                                contents[y++][x++] = c;
+                            }
+                        }
+                        case HORIZONTAL_INVERSE -> {
+                            for(char c : word.toCharArray()) {
+                                contents[y][x--] = c;
+                            }
+                        }
+                        case VERTICAL_INVERSE -> {
+                            for(char c : word.toCharArray()) {
+                                contents[y--][x] = c;
+                            }
+                        }
+                        case DIAGONAL_INVERSE -> {
+                            for(char c : word.toCharArray()) {
+                                contents[y--][x--] = c;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        fillWithRandomLetters(contents);
+        return contents;
+    }
+
+    public void displayGrid(char[][] contents) {
+        int gridSize = contents.length;
+        for(int i = 0; i < gridSize; i++) {
+            for(int j = 0; j < gridSize; j++) {
+                System.out.print(contents[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    //Fills any field not taken by a word with a random uppercase letter.
+    private void fillWithRandomLetters(char[][] contents) {
+        int gridSize = contents.length;
+        String uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i=0; i < gridSize; i++) {
+            for(int j=0; j < gridSize; j++) {
+                if(contents[i][j] == '_') {
+                    int randomIndex = ThreadLocalRandom.current().nextInt(0, uppercaseAlphabet.length());
+                    contents[i][j] = uppercaseAlphabet.charAt(randomIndex);
+                }
+            }
+        }
+    }
+
+    // Gives one of the 6 possible directions for the given word with the given starting coordinate. Returns null if word
+    // doesn't fit the grid boundaries in any of the directions.
+    private Direction getArbitraryValidDirection(char[][] contents, String word, Coordinate coordinate) {
+        List<Direction> directions = Arrays.asList(Direction.values());
+        Collections.shuffle(directions);
+        for(Direction direction : directions) {
+            if(doesFit(contents, word, coordinate, direction)) {
+                return direction;
+            }
+        }
+        return null;
+    }
+
+    // Checks if the given word stays within the grid when starting in the given coordinate and moving in the given direction.
+    private boolean doesFit(char[][] contents, String word, Coordinate coordinate, Direction direction) {
+        int gridSize = contents.length;
+        int wordLength = word.length();
+        switch (direction) {
+            case HORIZONTAL -> {
+                if(coordinate.x + wordLength > gridSize) return false;
+                for(int i=0; i < wordLength; i++) {
+                    if(contents[coordinate.y][coordinate.x + i] != '_') return false;
+                }
+            }
+            case VERTICAL -> {
+                if(coordinate.y + wordLength > gridSize) return false;
+                for(int i=0; i < wordLength; i++) {
+                    if(contents[coordinate.y + i][coordinate.x] != '_') return false;
+                }
+            }
+            case DIAGONAL -> {
+                if(coordinate.x + wordLength > gridSize || coordinate.y + wordLength > gridSize) return false;
+                for(int i=0; i < wordLength; i++) {
+                    if(contents[coordinate.y + i][coordinate.x + i] != '_') return false;
+                }
+            }
+            case HORIZONTAL_INVERSE -> {
+                if(coordinate.x < wordLength) return false;
+                for(int i=0; i < wordLength; i++) {
+                    if(contents[coordinate.y][coordinate.x - i] != '_') return false;
+                }
+            }
+            case VERTICAL_INVERSE -> {
+                if(coordinate.y < wordLength) return false;
+                for(int i=0; i < wordLength; i++) {
+                    if(contents[coordinate.y - i][coordinate.x] != '_') return false;
+                }
+            }
+            case DIAGONAL_INVERSE -> {
+                if(coordinate.x < wordLength || coordinate.y < wordLength) return false;
+                for(int i=0; i < wordLength; i++) {
+                    if(contents[coordinate.y - i][coordinate.x - i] != '_') return false;
+                }
+            }
+        }
+        return true;
+    }
+}
